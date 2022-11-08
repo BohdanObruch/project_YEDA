0064959200379fa2e2f3020a125d070c78a4b88f
Bodan1992 <bodan1992@gmail.com>
2022-11-08 10:35:46 +0300

    fix 2 tests(change locator)

--- diploma_project_tests/pages/favorite_list_page.py
+++ diploma_project_tests/pages/favorite_list_page.py
@@ -124,14 +124,11 @@ class AddingToFavoriteList:
         return self
 
     def add_second_hotel_to_favorites(self, value):
-        s((AppiumBy.XPATH, '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android'
-                           '.widget.FrameLayout/android.widget.LinearLayout/android.widget'
-                           '.FrameLayout '
-                           '/android.view.ViewGroup/android.widget.FrameLayout/android.widget'
-                           '.LinearLayout/android.view.ViewGroup/android.widget.FrameLayout/androidx'
-                           '.recyclerview.widget.RecyclerView/android.view.ViewGroup['
-                           '2]/android.view.ViewGroup/android.view.ViewGroup['
-                           '2]/android.view.ViewGroup[ '
+        s((AppiumBy.XPATH, '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget'
+                           '.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view'
+                           '.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.view.ViewGroup'
+                           '/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.view'
+                           '.ViewGroup[3]/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup['
                            '1]/android.view.ViewGroup/android.widget.ImageView')).click()
 
         s((AppiumBy.ID, 'com.booking:id/snackbar_text')).should(have.text(value))
