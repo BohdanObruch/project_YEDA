b17fed5c3a9d0a265e2be1a6397cf00e115a019a
Bodan1992 <bodan1992@gmail.com>
2022-12-05 00:10:38 +0300

    fixed api bundle test

--- tests/api/test_display_information_bundle.py
+++ tests/api/test_display_information_bundle.py
@@ -10,13 +10,13 @@ from allure import tag, title
 @tag('API')
 @title("Displaying the bundle on the website")
 def test_display_information_bundle():
-    id_college = os.getenv('ID_COLLEGE')
+    college_id = os.getenv('ID_COLLEGE')
     name_bundle = os.getenv('NAME_BUNDLE')
 
-    id_college = f'current_college_id={id_college}'
+    id_college = f'current_college_id={college_id}'
 
     response = yeda().get(
-        f'/wl/colleges/{id_college}/bundle/{name_bundle}', params=id_college
+        f'/wl/colleges/{college_id}/bundle/{name_bundle}', params=id_college
     )
     assert response.status_code == 200
     assert S(bundle) == response.json()
