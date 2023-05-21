d3316a358db83b48d3e52a6ea6798161aa50704d
Bodan1992 <bodan1992@gmail.com>
2023-05-21 11:43:03 +0300

    delete some tests

--- diploma_project_tests/helpers/app.py
+++ diploma_project_tests/helpers/app.py
@@ -1,12 +1,3 @@
-from diploma_project_tests.pages.web.bundle_page import CreateBundlesPage, FillingBundlesPage
-from diploma_project_tests.pages.web.lesson_page import LessonPage
-from diploma_project_tests.pages.web.user_page import CreateUserPage
-from diploma_project_tests.pages.web.teacher_page import CreateTeacherPage
-from diploma_project_tests.pages.web.questionnaire_page import CreateQuestionnairePage
-from diploma_project_tests.pages.web.questionnaire_page import FillingQuestionnairePage
-from diploma_project_tests.pages.web.course_page import CreateCoursePage
-from diploma_project_tests.pages.web.article_page import CreateArticlePage
-from diploma_project_tests.pages.web.survey_page import SurveyPage
 from diploma_project_tests.pages.mobile.favorite_list_page import *
 from diploma_project_tests.pages.mobile.registration_user_page import RegistrationUserPage
 from diploma_project_tests.pages.mobile.taxi_page import SearchTaxiPage
@@ -16,16 +7,6 @@ from diploma_project_tests.pages.mobile.attractions_page import SearchAttraction
 from diploma_project_tests.pages.mobile.destination_page import SearchDestinationPage
 
 
-create_bundles = CreateBundlesPage()
-filling_bundles = FillingBundlesPage()
-lesson_page = LessonPage()
-create_user = CreateUserPage()
-create_teacher = CreateTeacherPage()
-create_questionnaire = CreateQuestionnairePage()
-filling_questionnaire = FillingQuestionnairePage()
-create_course = CreateCoursePage()
-create_article = CreateArticlePage()
-survey_page = SurveyPage()
 create_list = CreateListPage()
 adding_to_favorite_list = AddingToFavoriteListPage()
 deleting_from_favorite_list = DeletingFromFavoriteListPage()
--- tests/conftest.py
+++ tests/conftest.py
@@ -20,103 +20,14 @@ def env():
 
 dotenv = dotenv_values()
 
-web_url = dotenv.get('WEB_URL')
-admin_panel_url = dotenv.get('ADMIN_PANEL_URL')
-id_college = dotenv.get('ID_COLLEGE')
-
-
-def opened_page_website():
-    browser.open(web_url)
-
-
-def opened_page_admin_panel():
-    browser.open(admin_panel_url)
-    browser.config.driver.maximize_window()
-
-
-@pytest.fixture(scope='function', autouse=True)
-def browser_management():
-    browser.config.base_url = os.getenv('selene.base_url', web_url)
-    browser.config.browser_name = os.getenv('selene.browser_name', 'chrome')
-    browser.config.hold_browser_open = (
-            os.getenv('selene.hold_browser_open', 'false').lower() == 'true'
-    )
-    browser.config.timeout = float(os.getenv('selene.timeout', '3'))
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
-
-
-DEFAULT_BROWSER_VERSION = "110.0"
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        '--browser_version',
-        default='110.0'
-    )
-
-
-@pytest.fixture(scope='function')
-def setup_browser(request):
-    browser_version = request.config.getoption('--browser_version')
-    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
-    options = Options()
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": browser_version,
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
-        }
-    }
-    options.capabilities.update(selenoid_capabilities)
-
-    url = os.getenv('URL')
-
-    driver = webdriver_selenium.Remote(
-        command_executor=f"{url}/wd/hub",
-        options=options
-    )
-    browser.config.driver = driver
-    yield browser
-
-    attach.add_html(browser)
-    attach.add_screenshot(browser)
-    attach.add_logs(browser)
-    attach.add_video_selenoid(browser)
-    browser.quit()
-
-
-# @pytest.fixture()
-# def register_user():
-#     data = user_api.create_user()
-#     current_college_id = f'current_college_id={id_college}'
-#
-#     response = yeda().post('/auth/register',
-#                            params=current_college_id,
-#                            data=data
-#                            )
-#
-#     response_parse = json.loads(str(response.text))
-#
-#     token = response_parse['auth']['access_token']
-#     user_name = response_parse["user"]["name"]
-#     user_email = response_parse["user"]["email"]
-#     user_pass = data["password"]
-#     id = response_parse["user"]["id"]
-#
-#     return token, user_name, user_email, user_pass, id
-
-
 @pytest.fixture(scope='function')
 def setup():
     linkApp = os.getenv('LINK_APP')
 
     desired_capabilities = ({
-        "platformName": "android",
-        "platformVersion": "9.0",
-        "deviceName": "Samsung Galaxy S20",
-        "os_version": "10.0",
+        "platformName": "ios",
+        "platformVersion": "13.1",
+        "deviceName": "iPhone 11 Pro Max",
         "app": f'{linkApp}',
         "build": "browserstack-build-" + str(date.today()),
         'bstack:options': {
@@ -124,6 +35,8 @@ def setup():
             "projectName": "Booking dev",
         }
     })
+
+
 
     userName = os.getenv('LOGIN')
     accessKey = os.getenv('KEY')
